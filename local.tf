d187917ce777a731ec2bee162bf03aa9a239e166
Shahnoor Khalidi <shahnoorrahimkhalidi@gmail.com>
2022-03-16 16:36:57 +0500

    steps-added

--- local.tf
+++ local.tf
@@ -1,4 +1,5 @@
  resource "local_file" "local-file" {
     filename = "/home/shahnoor/Desktop/terraform/local-file.txt"
     content = "the content is within the file 'local-file.txt"
+    file_permission = "0700"
 }
